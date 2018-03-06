f325b6a673d3a603c9c556addb20e1574393060d
Keith Stewart <kgstewart@gmail.com>
2018-03-06 05:39:31 -0600

    #4: fix unit tests

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatCardModule, MatInputModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 
 import { HeroSearchComponent } from './hero-search.component';
@@ -16,7 +18,11 @@ describe('HeroSearchComponent', () => {
         RouterTestingModule,
         StoreModule.forRoot({
           ...fromHero.reducers
-        })
+        }),
+        MatFormFieldModule,
+        MatCardModule,
+        MatInputModule,
+        NoopAnimationsModule
       ]
     })
     .compileComponents();
--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 
 import { HeroesComponent } from './heroes.component';
@@ -16,7 +18,10 @@ describe('HeroesComponent', () => {
         RouterTestingModule,
         StoreModule.forRoot({
           ...fromHero.reducers
-        })
+        }),
+        MatFormFieldModule,
+        MatInputModule,
+        NoopAnimationsModule
       ]
     })
     .compileComponents();
