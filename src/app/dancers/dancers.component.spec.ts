a9f9558217e85a53d20bba230f35a371565b3df2
Keith Stewart <kgstewart@gmail.com>
2018-03-14 05:14:24 -0500

    fix error in test

--- src/app/dancers/dancers.component.spec.ts
+++ src/app/dancers/dancers.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatCardModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 
 import { DancersComponent } from './dancers.component';
@@ -21,6 +21,7 @@ describe('DancersComponent', () => {
         }),
         MatFormFieldModule,
         MatInputModule,
+        MatCardModule,
         NoopAnimationsModule
       ]
     })
