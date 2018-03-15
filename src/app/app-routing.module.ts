479d077205bfd4341f6a0c25fac0052eb7e3467f
Keith Stewart <kgstewart@gmail.com>
2018-03-15 17:39:42 -0500

    #22: create route to dance challenge component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DancersComponent } from './dancers/dancers.component';
 import { DancerDetailComponent } from './dancer-detail/dancer-detail.component';
+import { DanceChallengeComponent } from './dance-challenge/dance-challenge.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: DancerDetailComponent },
-  { path: 'dancers', component: DancersComponent }
+  { path: 'dancers', component: DancersComponent },
+  { path: 'challenge/:id', component: DanceChallengeComponent }
 ];
 
 @NgModule({
